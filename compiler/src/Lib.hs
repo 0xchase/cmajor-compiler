8f4341eea2c5c009da21af89ca333f0861b3fe99
0xchase <chasekanipe@gmail.com>
2023-04-11 21:37:19 -0400

    Added some haskell source

---
+++ compiler/src/Lib.hs
@@ -0,0 +1,42 @@
+module Lib
+    ( someFunc
+    ) where
+
+import Data.Text ( Text )
+import Data.Char ( chr )
+
+someFunc :: IO ()
+someFunc = putStrLn "someFunc"
+
+data Op = Add
+    | Sub
+    | Mult
+    | Div
+    deriving (Show, Eq)
+
+data Uop = Neg
+    | Not
+    deriving (Show, Eq)
+
+data Expr = Literal Int
+    | Strlit Text
+    | CharLit Int
+    | Fliteral Double
+    | BoolLit Bool
+    deriving (Show, Eq)
+
+data Statement = Expr Expr
+    | Block [Statement]
+    | Return Expr
+    | If Expr Statement Statement
+    | For Expr Expr Expr Statement
+    | while Expr Statement
+    deriving (Show, Eq)
+
+data Type = Pointer Type
+    | TyInt
+    | TyBool
+    | TyFloat
+    | TyChar
+    deriving (Show, Eq)
+
